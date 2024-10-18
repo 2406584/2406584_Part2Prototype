09ca5048fa12a7bfc136b3a4bb86dea2fcac21cc
2406584 <2406584@abertay.ac.uk>
2024-10-18 14:37:34 +0100

    Committing first batch of code for testing

--- src/AssetTrackingApp.java
+++ src/AssetTrackingApp.java
@@ -191,6 +191,7 @@ public class AssetTrackingApp extends JFrame {
         }
     }
 
+    // main method
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
